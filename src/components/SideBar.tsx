c96af64c3a0c5ee568a6be5de700fa0183c9b8ee
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-17 14:14:55 -0300

    Fix bug in login functionality
    Refactor code for improved performance
    
    Add new feature for user authentication
    
    Update UI for better user experience
    
    Fix typo in variable name
    
    Optimize database queries for faster response time
    
    Add error handling for edge cases
    
    Update dependencies to latest versions
    
    Remove unused code
    
    Improve code readability and maintainability

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import {
   FaFileMedicalAlt,
@@ -11,18 +11,26 @@ import {
 } from "react-icons/fa";
 import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { jwtDecode } from "jwt-decode";
 import { FiLogOut } from "react-icons/fi";
+
+interface MyTokenPayload {
+  roles: string[];
+}
 
 export default function SideBar() {
   const router = useRouter();
   const [isSidebarOpen, setSidebarOpen] = useState(false);
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-  // const { data: session } = useSession();
+  const [userRoles, setUserRoles] = useState<string[]>([]);
+  const { data: session } = useSession();
 
-  // const esPaciente = session?.user.roles.includes("Paciente");
-  // const esMedico = session?.user.roles.includes("Médico");
-  // const esAdmin = session?.user.roles.includes("Administrador");
-  // const esSecretaria = session?.user.roles.includes("Secretaria");
+  useEffect(() => {
+    if (session?.user?.token) {
+      const decoded = jwtDecode<MyTokenPayload>(session.user.token);
+      setUserRoles(decoded.roles || []);
+    }
+  }, [session?.user?.token]);
 
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
@@ -73,141 +81,142 @@ export default function SideBar() {
           </div>
         </div>
       </nav>
-
-      <aside
-        id="logo-sidebar"
-        className={`fixed top-20 left-0 z-40 sm:w-64 h-screen pt-20 transition-transform duration-300 ease-in-out bg-white border-r border-gray-200 sm:translate-x-0 ${
-          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
-        aria-label="Sidebar"
-      >
-        <div className="h-full px-3 pb-4 overflow-y-auto bg-white">
-          <ul className="space-y-2 font-medium">
-            <li>
-              <a
-                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group cursor-pointer"
-                onClick={() => router.push("/home")}
-              >
-                <FaHome size={25} color="#0f766e" />
-                <span className="flex-1 ml-3 whitespace-nowrap">Inicio</span>
-              </a>
-            </li>
-            <li>
-              <a
-                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group cursor-pointer"
-                onClick={() => router.push("/dashboard")}
-              >
-                <FaUser size={25} color="#0f766e" />
-                <span className="flex-1 ml-3 whitespace-nowrap">Perfil</span>
-              </a>
-            </li>
-
-            {/* {esPaciente && ( */}
-              <>
-                <li>
-                  <a
-                    onClick={() => router.push("/estudios")}
-                    className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 group cursor-pointer"
-                  >
-                    <FaFileMedicalAlt size={25} color="#0f766e" />
-                    <span className="ml-3">Estudios</span>
-                  </a>
-                </li>
-              </>
-            {/* )} */}
-
-            {/* {esSecretaria && ( */}
-              <>
-                <li>
-                  <a
-                    onClick={() => router.push("/secretaria")}
-                    className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100  group cursor-pointer"
-                  >
-                    <FaFileMedicalAlt size={25} color="#0f766e" />
-                    <span className="ml-3">Panel Secretaria</span>
-                  </a>
-                </li>
-              </>
-            {/* )} */}
-
-            {/* {esMedico && ( */}
-              <>
-                <li>
-                  <button
-                    type="button"
-                    className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100"
-                    aria-controls="dropdown-example"
-                    onClick={() => setDropdownOpen(!isDropdownOpen)}
-                    data-collapse-toggle="dropdown-example"
-                  >
-                    <FaUsers size={25} color="#0f766e" />
-                    <span className="flex-1 ml-3 text-left whitespace-nowrap">
-                      Mis Pacientes
-                    </span>
-                    <svg
-                      className="w-3 h-3"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 10 6"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="m1 1 4 4 4-4"
-                      />
-                    </svg>
-                  </button>
-                  <ul
-                    id="dropdown-example"
-                    className={`${
-                      isDropdownOpen ? "block" : "hidden"
-                    } py-2 space-y-2`}
-                  >
-                    <li>
-                      <a
-                        onClick={() => router.push("/pacientes")}
-                        className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 cursor-pointer"
-                      >
-                        <FaUsers size={25} color="#0f766e" />
-                        <span className="ml-3">Lista de Pacientes</span>
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </>
-            {/* )} */}
-            {/* {esAdmin && ( */}
+      {session && (
+        <aside
+          id="logo-sidebar"
+          className={`fixed top-20 left-0 z-40 sm:w-64 h-screen pt-20 transition-transform duration-300 ease-in-out bg-white border-r border-gray-200 sm:translate-x-0 ${
+            isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
+          aria-label="Sidebar"
+        >
+          <div className="h-full px-3 pb-4 overflow-y-auto bg-white">
+            <ul className="space-y-2 font-medium">
               <li>
                 <a
-                  onClick={() => router.push("/ajustes")}
-                  className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100  group cursor-pointer"
+                  className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group cursor-pointer"
+                  onClick={() => router.push("/home")}
                 >
-                  <FaUserLock size={25} color="#0f766e" />
-                  <span className="ml-3">Panel Admin</span>
+                  <FaHome size={25} color="#0f766e" />
+                  <span className="flex-1 ml-3 whitespace-nowrap">Inicio</span>
                 </a>
               </li>
-            {/* )} */}
+              <li>
+                <a
+                  className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100  group cursor-pointer"
+                  onClick={() => router.push("/dashboard")}
+                >
+                  <FaUser size={25} color="#0f766e" />
+                  <span className="flex-1 ml-3 whitespace-nowrap">Perfil</span>
+                </a>
+              </li>
 
-            <hr />
-            <li>
-              <a
-                onClick={() =>
-                  signOut({ callbackUrl: process.env.NEXT_PUBLIC_BASE_URL })
-                }
-                className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-red-100  group cursor-pointer"
-              >
-                <FiLogOut size={25} />
-                <span className="flex-1 ml-3 whitespace-nowrap">
-                  Cerrar Sesión
-                </span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </aside>
+              <>
+                {userRoles.includes("Paciente") && (
+                  <li>
+                    <a
+                      onClick={() => router.push("/estudios")}
+                      className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group cursor-pointer"
+                    >
+                      <FaFileMedicalAlt size={25} color="#0f766e" />
+                      <span className="ml-3">Estudios</span>
+                    </a>
+                  </li>
+                )}
+              </>
+
+              {userRoles.includes("Secretaria") && (
+                <>
+                  <li>
+                    <a
+                      onClick={() => router.push("/secretaria")}
+                      className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100  group cursor-pointer"
+                    >
+                      <FaFileMedicalAlt size={25} color="#0f766e" />
+                      <span className="ml-3">Panel Secretaria</span>
+                    </a>
+                  </li>
+                </>
+              )}
+
+              {userRoles.includes("Médico") && (
+                <>
+                  <li>
+                    <button
+                      type="button"
+                      className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100"
+                      aria-controls="dropdown-example"
+                      onClick={() => setDropdownOpen(!isDropdownOpen)}
+                      data-collapse-toggle="dropdown-example"
+                    >
+                      <FaUsers size={25} color="#0f766e" />
+                      <span className="flex-1 ml-3 text-left whitespace-nowrap">
+                        Mis Pacientes
+                      </span>
+                      <svg
+                        className="w-3 h-3"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 10 6"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="m1 1 4 4 4-4"
+                        />
+                      </svg>
+                    </button>
+                    <ul
+                      id="dropdown-example"
+                      className={`${
+                        isDropdownOpen ? "block" : "hidden"
+                      } py-2 space-y-2`}
+                    >
+                      <li>
+                        <a
+                          onClick={() => router.push("/pacientes")}
+                          className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 cursor-pointer"
+                        >
+                          <FaUsers size={25} color="#0f766e" />
+                          <span className="ml-3">Lista de Pacientes</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                </>
+              )}
+              {userRoles.includes("Administrador") && (
+                <li>
+                  <a
+                    onClick={() => router.push("/ajustes")}
+                    className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100  group cursor-pointer"
+                  >
+                    <FaUserLock size={25} color="#0f766e" />
+                    <span className="ml-3">Panel Admin</span>
+                  </a>
+                </li>
+              )}
+
+              <hr />
+              <li>
+                <a
+                  onClick={() =>
+                    signOut({ callbackUrl: process.env.NEXT_PUBLIC_BASE_URL })
+                  }
+                  className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-red-100  group cursor-pointer"
+                >
+                  <FiLogOut size={25} />
+                  <span className="flex-1 ml-3 whitespace-nowrap">
+                    Cerrar Sesión
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </aside>
+      )}
     </>
   );
 }
