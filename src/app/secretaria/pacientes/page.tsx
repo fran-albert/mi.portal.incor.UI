a0c0a94756c6863ea107b24da6e7cd1d7c52c910
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-20 19:54:23 -0300

    Add secretaria/medicos/page.tsx and secretaria/pacientes/page.tsx

---
+++ src/app/secretaria/pacientes/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import SideBar from "@/components/SideBar";
+import PacientesTabla from "./pacientesTabla";
+// import withSessionTimeout from "@/components/withSessionTimeout";
+const SecretariaPage = () => {
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="md:w-64 w-full">
+        <SideBar />
+      </div>
+      <div className="flex-grow">
+        <PacientesTabla />
+      </div>
+    </div>
+  );
+};
+
+// export default withSessionTimeout(SecretariaPage);
+export default SecretariaPage;
