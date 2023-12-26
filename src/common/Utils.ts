bc2fe02f1058243726ba9752b60d87333eab9f83
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-26 15:19:01 -0300

    Add formatDateToUTCString function to Utils.ts

--- src/common/Utils.ts
+++ src/common/Utils.ts
@@ -13,3 +13,14 @@ export function formatearDNI(dni: string): string {
   let dniConPuntos = dniReversed.match(/.{1,3}/g)?.join(".") || "";
   return dniConPuntos.split("").reverse().join("");
 }
+
+export function formatDateToUTCString(date: Date | null): string {
+  if (!date) return "";
+
+  // Convierte la fecha a UTC y formatea como una cadena
+  const year = date.getUTCFullYear();
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+  const day = date.getUTCDate().toString().padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+}
