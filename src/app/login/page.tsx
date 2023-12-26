037c9aa9d620445da37f87b76f06f730cc727e9d
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-26 15:18:22 -0300

    Refactor login page to use custom input and label components

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,6 +6,8 @@ import { Input, Button, Divider, Checkbox } from "@nextui-org/react";
 import { EyeFilledIcon } from "../../components/EyeFilledIcon";
 import { EyeSlashFilledIcon } from "../../components/EyeSlashFilledIcon";
 import SideBar from "@/components/SideBar";
+import { CustomInput } from "@/components/ui/Input";
+import { CustomLabel } from "@/components/ui/Label";
 
 function LoginPage() {
   const [errors, setErrors] = useState<string[]>([]);
@@ -48,74 +50,29 @@ function LoginPage() {
             <h1 className="text-lg md:text-2xl font-bold text-center">
               Iniciar Sesión
             </h1>
-            <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-              <Input
-                type="email"
-                id="email"
-                name="email"
-                color="default"
-                label="Correo Electrónico"
-                required
-                variant="faded"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-                placeholder="Ingrese su correo electrónico"
-                classNames={{
-                  label: "text-black/50 ",
-                  input: ["bg-transparent", "text-black/90 "],
-                  innerWrapper: "bg-transparent",
-                  inputWrapper: [
-                    "bg-white-500/50",
-                    "backdrop-blur-xl",
-                    "backdrop-saturate-200",
-                    "!cursor-text",
-                    "hover:bg-white-500/50",
-                    "focus:bg-white-500/50",
-                    "group-data-[focused=true]:bg-white-500/50",
-                  ],
-                }}
-              />
+            <div className="flex flex-col gap-4">
+              <div className="flex w-full flex-wrap gap-2">
+                <CustomLabel htmlFor="email">Correo Electrónico</CustomLabel>
+                <CustomInput
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+              <div className="flex w-full flex-wrap gap-2">
+                <CustomLabel htmlFor="password">Password</CustomLabel>
+                <CustomInput
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </div>
             </div>
-            <div>
-              <Input
-                label="Contraseña"
-                id="password"
-                name="password"
-                required
-                placeholder="Ingrese su contraseña"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-                variant="faded"
-                classNames={{
-                  label: "text-black/50 ",
-                  input: ["bg-transparent", "text-black/90 "],
-                  innerWrapper: "bg-transparent",
-                  inputWrapper: [
-                    "bg-white-500/50",
-                    "backdrop-blur-xl",
-                    "backdrop-saturate-200",
-                    "!cursor-text",
-                    "hover:bg-white-500/50",
-                    "focus:bg-white-500/50",
-                    "group-data-[focused=true]:bg-white-500/50",
-                  ],
-                }}
-                endContent={
-                  <button
-                    className="focus:outline-none"
-                    type="button"
-                    onClick={toggleVisibility}
-                  >
-                    {isVisible ? (
-                      <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                    ) : (
-                      <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                    )}
-                  </button>
-                }
-                type={isVisible ? "text" : "password"}
-              />
-            </div>
+
             {errors.length > 0 && (
               <div className="alert alert-danger mt-2">
                 <ul className="mb-0 text-red-500">
