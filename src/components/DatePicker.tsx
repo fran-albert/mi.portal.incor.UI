639b2f09a5e4550f76b4f6caa6d0128f700ae5d5
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-27 17:00:22 -0300

    Refactor DatePicker component button element

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -13,8 +13,8 @@ const MyDatePicker = ({ onChange, selectedDate }: MyDatePickerProps) => {
     HTMLButtonElement,
     ExampleCustomInputProps
   >(({ value, onClick }, ref) => (
-    <Button
-      className="block w-full rounded-lg border-0 py-1.5 text-gray-900 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-800 sm:text-sm sm:leading-6d"
+    <button
+      className="block w-full rounded-lg border-0 py-1.5 text-gray-900 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-teal-800 sm:text-sm sm:leading-6 px-3"
       onClick={(e) => {
         e.preventDefault();
         onClick?.(e);
@@ -22,8 +22,9 @@ const MyDatePicker = ({ onChange, selectedDate }: MyDatePickerProps) => {
       ref={ref}
     >
       {value}
-    </Button>
+    </button>
   ));
+  
 
   ExampleCustomInput.displayName = "ExampleCustomInput";
 
