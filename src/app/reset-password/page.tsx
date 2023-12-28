99d157cf9053c68a4814825b07be8e949599db74
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-27 23:26:06 -0300

    Add reset password page component

---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import SideBar from "@/components/SideBar";
+import NewPasswordComponent from "./newPasswordComponent";
+
+const ForgotPassword = () => {
+  return (
+    <>
+      <div className="bg-gray-50 min-h-screen">
+        <SideBar />
+        <div className="py-12 mt-32">
+          <div className="flex flex-wrap justify-center px-4 space-x-0 sm:space-x-8">
+            <div className="w-full md:w-1/2 lg:w-1/3 p-2">
+              <NewPasswordComponent />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ForgotPassword;
