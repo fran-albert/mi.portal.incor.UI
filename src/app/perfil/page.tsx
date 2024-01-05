8163c7cf8ecd1bda96651c1d0334af94ab2edb59
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-01-05 14:02:53 -0300

    Add user state and profile fetching to Profile page

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -2,8 +2,33 @@
 
 import SideBar from "@/components/SideBar";
 import UserCardComponent from "./userCard";
+import { useEffect, useState } from "react";
+import { IUser } from "@/common/interfaces/user.interface";
+import { useCustomSession } from "@/context/SessionAuthProviders";
+import useFetchStates from "@/hooks/useFetchState";
+import useFetchProfile from "@/hooks/useFetchProfile";
+import axios from "axios";
 
 const Profile = () => {
+  const [user, setUser] = useState<IUser | undefined>(undefined);
+  const { session } = useCustomSession();
+  const { states: provincias, isLoading: isLoadingStates } = useFetchStates(
+    session?.user?.token
+  );
+  const { profile, isLoading: isProfileLoading } = useFetchProfile(
+    session?.user?.token
+  );
+
+  useEffect(() => {
+    if (profile) {
+      setUser(profile);
+    }
+  }, [profile]);
+
+  const updateUser = (updatedUser: IUser) => {
+    setUser(updatedUser);
+  };
+
   return (
     <>
       <div className="flex flex-col md:flex-row">
@@ -11,7 +36,11 @@ const Profile = () => {
           <SideBar />
         </div>
         <div className="flex-grow">
-          <UserCardComponent />
+          <UserCardComponent
+            user={user}
+            states={provincias}
+            updateUser={updateUser}
+          />
         </div>
       </div>
     </>
