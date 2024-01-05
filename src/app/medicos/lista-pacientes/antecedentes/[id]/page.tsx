cc0521b6240a965dfd79872682a318318cae3749
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-01-05 14:02:24 -0300

    Add HistoryUser component to HistoryPatientPage

--- src/app/medicos/lista-pacientes/antecedentes/[id]/page.tsx
+++ src/app/medicos/lista-pacientes/antecedentes/[id]/page.tsx
@@ -1,16 +1,29 @@
 "use client";
 import SideBar from "@/components/SideBar";
+import HistoryUser from "./history.user";
+import { useCustomSession } from "@/context/SessionAuthProviders";
+import useFetchUser from "@/hooks/useFetchUser";
+import LoadingPage from "@/components/Loading";
 // import withSessionTimeout from "@/components/withSessionTimeout";
-function StudiesPatientsPage() {
+function HistoryPatientPage({ params }: { params: any }) {
+  const idPatient = parseInt(params.id);
+  const { session } = useCustomSession();
+  const { user, isLoading } = useFetchUser(session?.user?.token, idPatient);
+
+  if (isLoading) {
+    return <LoadingPage props={"Cargando datos del paciente..."} />;
+  }
   return (
     <div className="flex flex-col md:flex-row">
       <div className="md:w-64 w-full">
         <SideBar />
       </div>
-      <div className="flex-grow"></div>
+      <div className="flex-grow mt-40">
+        <HistoryUser paciente={user} />
+      </div>
     </div>
   );
 }
 
 // export default withSessionTimeout(Pacientes);
-export default StudiesPatientsPage;
+export default HistoryPatientPage;
