17f21e54c689b942793181fe306060e879daf4ae
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-01-08 15:13:26 -0300

    Refactor StudiesTable component to make it responsive

--- src/app/estudios/studies.table.tsx
+++ src/app/estudios/studies.table.tsx
@@ -53,68 +53,70 @@ export default function StudiesTable() {
 
   return (
     <div className="flex justify-center p-4 mt-40">
-      <div className="w-1/2">
+      <div className="w-full lg:w-1/2">
         <h2 className="text-3xl font-bold text-center mb-4">Tus Estudios</h2>
         {estudios && estudios.length > 0 ? (
           <>
-            <Table
-              aria-label="Descripción de la tabla"
-              bottomContent={
-                <div className="flex w-full justify-center">
-                  <Paginacion
-                    totalItems={totalEstudios}
-                    itemsPerPage={itemsPorPagina}
-                    currentPage={paginaActual}
-                    total={totalPages}
-                    setCurrentPage={setPaginaActual}
-                    onChange={handlePageChange}
-                  />
-                </div>
-              }
-              classNames={{
-                wrapper: "min-h-[222px]",
-              }}
-            >
-              <TableHeader columns={columns}>
-                {(column: ITableColumn) => (
-                  <TableColumn key={column.key}>{column.label}</TableColumn>
-                )}
-              </TableHeader>
-              <TableBody>
-                {estudiosMostrados &&
-                  estudiosMostrados.map((estudio, index) => (
-                    <TableRow
-                      key={estudio.id}
-                      className="transition-all hover:bg-gray-100 dark:border-black-700 dark:bg-black-800"
-                    >
-                      <TableCell className="py-3 text-gray-900 text-base">
-                        {(paginaActual - 1) * itemsPorPagina + (index + 1)}
-                      </TableCell>
-                      <TableCell className="py-3 text-gray-900 text-base font-medium">
-                        {estudio.name}
-                      </TableCell>
-                      <TableCell className="py-3 text-gray-900 text-base">
-                        {formatDate(estudio.date)}
-                      </TableCell>
-                      <TableCell className="p-4">
-                        <div className="relative flex justify-around items-center gap-1">
-                          <Tooltip content="Ver PDF">
-                            <span className="text-3xl cursor-pointer active:opacity-50">
-                              <a
-                                href={`https://incor-ranking.s3.us-east-1.amazonaws.com/storage/laboratorios/${estudio.file}.pdf`}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                <FaFilePdf size={30} color="#0d9488" />
-                              </a>
-                            </span>
-                          </Tooltip>
-                        </div>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-              </TableBody>
-            </Table>
+            <div className="overflow-x-auto">
+              <Table
+                aria-label="Descripción de la tabla"
+                bottomContent={
+                  <div className="flex w-full justify-center">
+                    <Paginacion
+                      totalItems={totalEstudios}
+                      itemsPerPage={itemsPorPagina}
+                      currentPage={paginaActual}
+                      total={totalPages}
+                      setCurrentPage={setPaginaActual}
+                      onChange={handlePageChange}
+                    />
+                  </div>
+                }
+                classNames={{
+                  wrapper: "min-h-[222px] w-full",
+                }}
+              >
+                <TableHeader columns={columns}>
+                  {(column: ITableColumn) => (
+                    <TableColumn key={column.key}>{column.label}</TableColumn>
+                  )}
+                </TableHeader>
+                <TableBody>
+                  {estudiosMostrados &&
+                    estudiosMostrados.map((estudio, index) => (
+                      <TableRow
+                        key={estudio.id}
+                        className="transition-all hover:bg-gray-100 dark:border-black-700 dark:bg-black-800"
+                      >
+                        <TableCell className="py-3 text-gray-900 text-base">
+                          {(paginaActual - 1) * itemsPorPagina + (index + 1)}
+                        </TableCell>
+                        <TableCell className="py-3 text-gray-900 text-base font-medium">
+                          {estudio.name}
+                        </TableCell>
+                        <TableCell className="py-3 text-gray-900 text-base">
+                          {formatDate(estudio.date)}
+                        </TableCell>
+                        <TableCell className="p-4">
+                          <div className="relative flex justify-around items-center gap-1">
+                            <Tooltip content="Ver PDF">
+                              <span className="text-3xl cursor-pointer active:opacity-50">
+                                <a
+                                  href={`https://incor-ranking.s3.us-east-1.amazonaws.com/storage/laboratorios/${estudio.file}.pdf`}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                >
+                                  <FaFilePdf size={30} color="#0d9488" />
+                                </a>
+                              </span>
+                            </Tooltip>
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </div>
           </>
         ) : (
           <div className="text-center">
