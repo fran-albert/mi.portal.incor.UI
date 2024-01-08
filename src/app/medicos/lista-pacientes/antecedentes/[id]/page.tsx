73a809355a4be4a5e5150d668812537e7a649163
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-01-08 15:13:31 -0300

    Add header component and update main content area

--- src/app/medicos/lista-pacientes/antecedentes/[id]/page.tsx
+++ src/app/medicos/lista-pacientes/antecedentes/[id]/page.tsx
@@ -4,6 +4,7 @@ import HistoryUser from "./history.user";
 import { useCustomSession } from "@/context/SessionAuthProviders";
 import useFetchUser from "@/hooks/useFetchUser";
 import LoadingPage from "@/components/Loading";
+import HeaderPatient from "@/components/ui/Header";
 // import withSessionTimeout from "@/components/withSessionTimeout";
 function HistoryPatientPage({ params }: { params: any }) {
   const idPatient = parseInt(params.id);
@@ -18,8 +19,15 @@ function HistoryPatientPage({ params }: { params: any }) {
       <div className="md:w-64 w-full">
         <SideBar />
       </div>
-      <div className="flex-grow mt-40">
-        <HistoryUser paciente={user} />
+      <div className="flex-grow">
+        <HeaderPatient user={user} /> 
+        <main className="flex-grow p-4 flex justify-center">
+          <div className="flex flex-col items-center">
+            <div className="w-full">
+              <p className="text-center">Contenido de la página...</p>
+            </div>
+          </div>
+        </main>
       </div>
     </div>
   );
