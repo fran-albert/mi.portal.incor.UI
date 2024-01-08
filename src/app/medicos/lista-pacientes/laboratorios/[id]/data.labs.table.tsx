1b756306835c3795152094e35058cdca65eb7447
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-01-08 15:13:46 -0300

    Update user authentication logic

--- src/app/medicos/lista-pacientes/laboratorios/[id]/data.labs.table.tsx
+++ src/app/medicos/lista-pacientes/laboratorios/[id]/data.labs.table.tsx
@@ -1,21 +1,12 @@
 "use client";
-import {
-  Table,
-  TableHeader,
-  TableColumn,
-  TableBody,
-  TableRow,
-  TableCell,
-} from "@nextui-org/react";
 import React, { useEffect, useMemo, useState } from "react";
-import { createTableColumns, formatDate } from "@/common/Utils";
-import { ITableColumn } from "@/common/interfaces/table.column.interface";
-import { Estudio } from "@/common/interfaces/study.interface";
+import { formatDate } from "@/common/Utils";
+import { useTable, ColumnInstance } from "react-table";
+import { CustomInput } from "@/components/ui/Input";
+import { IUser } from "@/common/interfaces/user.interface";
 
-interface DatosEditables {
-  [date: string]: {
-    [prueba: string]: string;
-  };
+interface RowData {
+  [key: string]: string | number | boolean | undefined;
 }
 
 interface Lab {
@@ -24,12 +15,16 @@ interface Lab {
   date: string;
   file: string;
   deletedAt: string | null;
-  datalab: Datalab;
+  datalab: DataLab;
 }
 
-interface Datalab {
-  id: number;
+interface CustomColumnInstance<D extends object = {}>
+  extends ColumnInstance<D> {
+  isEditable?: boolean;
+}
 
+interface DataLab {
+  [key: string]: string | undefined;
   ldh: string;
   lonogramaSangre: string;
   colesterolHdl: string;
@@ -43,133 +38,164 @@ interface Datalab {
   otros: string;
 }
 
-const tiposDePruebas = [
-  "ldh",
-  "lonogramaSangre",
-  "colesterolHdl",
-  "colesterolLdl",
-  "trigliceridos",
-  "uricemia",
-  "bilirrubina",
-  "colinesterasaSerica",
-  "gammaGlutamil",
-  "t4Libre",
-  "otros",
-];
+const testNames: { [key in keyof DataLab]?: string } = {
+  ldh: "LDH",
+  lonogramaSangre: "Ionograma Sangre",
+  colesterolHdl: "Colesterol HDL",
+  colesterolLdl: "Colesterol LDL",
+  trigliceridos: "Triglicéridos",
+  uricemia: "Uricemia",
+  bilirrubina: "Bilirrubina",
+  colinesterasaSerica: "Colinesterasa Sérica",
+  gammaGlutamil: "Gamma Glutamil",
+  t4Libre: "T4 Libre",
+  otros: "Otros",
+};
+
+interface EditableCellProps {
+  value: string | number;
+  row: {
+    index: number;
+  };
+  column: {
+    id: string;
+  };
+  updateMyData: (index: number, id: string, value: string) => void;
+}
+
+const updateMyData = (index: number, id: string, value: string | number) => {
+  // lógica de actualización aquí
+};
+
+const EditableCell: React.FC<EditableCellProps> = ({
+  value: initialValue,
+  row: { index },
+  column: { id },
+  updateMyData,
+}) => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const onBlur = () => {
+    updateMyData(index, id, value.toString());
+  };
+
+  useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  return <CustomInput value={value} onChange={onChange} onBlur={onBlur} />;
+};
 
 const DataLabsTable = ({ paciente }: any) => {
   const labs = useMemo(() => paciente?.labs || [], [paciente]);
-  const [uniqueDates, setUniqueDates] = useState<any[]>([]);
-  const [datosEditables, setDatosEditables] = useState<DatosEditables>({});
-  const [labDataByDate, setLabDataByDate] = useState<Map<string, any[]>>(
-    new Map()
-  );
 
-  useEffect(() => {
-    const dates = new Set(labs.map((lab: any) => lab.date.split("T")[0]));
-    setUniqueDates(Array.from(dates));
-  }, [labs]);
-
-  useEffect(() => {
-    const newLabDataByDate = new Map();
-    labs.forEach((lab: any) => {
-      const date = lab.date.split("T")[0];
-      if (!newLabDataByDate.has(date)) {
-        newLabDataByDate.set(date, []);
-      }
-      if (lab.datalab) {
-        newLabDataByDate.get(date).push(lab.datalab);
-      }
-    });
-    setLabDataByDate(newLabDataByDate);
-    setUniqueDates(Array.from(newLabDataByDate.keys()));
-  }, [labs]);
-
-  const columns = [
-    { key: "datos", label: "Datos" },
-    ...uniqueDates.map((date) => ({ key: date, label: date })),
-  ];
-
-  useEffect(() => {
-    const initialData: DatosEditables = {};
+  const data = useMemo(() => {
+    const rows: { [key: string]: RowData } = {};
     labs.forEach((lab: Lab) => {
-      const date = lab.date.split("T")[0];
-
       if (lab.datalab) {
-        tiposDePruebas.forEach((prueba) => {
-          if (!initialData[date]) {
-            initialData[date] = {};
+        const date = formatDate(lab.date);
+        const datalab: DataLab = lab.datalab;
+        Object.keys(datalab).forEach((key) => {
+          if (key !== "id" && key !== "lab") {
+            const testName = testNames[key] || key;
+            if (!rows[testName]) {
+              rows[testName] = { prueba: testName };
+            }
+            rows[testName][date] = datalab[key];
           }
-          const valor = lab.datalab[prueba as keyof Datalab];
-          initialData[date][prueba] = valor != null ? valor.toString() : "";
         });
       }
     });
-    setDatosEditables(initialData);
+
+    return Object.values(rows);
   }, [labs]);
 
-  const handleDataChange = (date: string, prueba: string, newValue: string) => {
-    const updatedData = { ...datosEditables };
+  const columns = useMemo(() => {
+    const dateColumns = labs.map((lab: Lab) => ({
+      Header: formatDate(lab.date),
+      accessor: formatDate(lab.date),
+      Cell: EditableCell,
+      isEditable: true,
+    }));
+    return [
+      {
+        Header: "Test",
+        accessor: "prueba",
+      },
+      ...dateColumns,
+    ];
+  }, [labs]);
 
-    if (!updatedData[date]) {
-      updatedData[date] = {};
-    }
-
-    updatedData[date][prueba] = newValue;
-    setDatosEditables(updatedData);
-  };
-
-  const renderEditableCell = (date: string, prueba: string) => {
-    const value =
-      datosEditables[date] && datosEditables[date][prueba]
-        ? datosEditables[date][prueba]
-        : "";
-    return (
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => handleDataChange(date, prueba, e.target.value)}
-        className="p-1 border rounded w-full"
-        style={{ maxWidth: "100px" }}
-      />
-    );
-  };
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+      // updateMyData,
+    });
 
   return (
-    <>
-      <div className="flex justify-center p-4">
-        <div className="w-1/2">
-          {labs.length > 0 && uniqueDates.length > 0 ? (
-            <Table
-              aria-label="Descripción de la tabla"
-              classNames={{ wrapper: "min-h-[222px]" }}
-            >
-              <TableHeader>
-                {columns.map((column) => (
-                  <TableColumn key={column.key}>{column.label}</TableColumn>
-                ))}
-              </TableHeader>
-              <TableBody>
-                {tiposDePruebas.map((prueba) => (
-                  <TableRow key={prueba}>
-                    <TableCell>{prueba}</TableCell>
-                    {/*{uniqueDates.map((date) => (
-                      <TableCell key={`${date}-${prueba}`}>
-                        {renderEditableCell(date, prueba)}
-                      </TableCell>
-                    ))} */}
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          ) : (
-            <div className="text-center">
-              <p className="text-gray-700 text-lg"></p>
-            </div>
-          )}
-        </div>
-      </div>
-    </>
+    <div className="overflow-x-auto shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+      <table
+        {...getTableProps()}
+        className="min-w-full divide-y divide-gray-300"
+      >
+        <thead className="bg-gray-50">
+          {headerGroups.map((headerGroup) => {
+            return (
+              <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+                {headerGroup.headers.map((column) => {
+                  return (
+                    <th
+                      {...column.getHeaderProps()}
+                      key={column.id} 
+                      scope="col"
+                      className="py-3 px-6 text-sm font-semibold text-gray-900 text-left"
+                    >
+                      {column.render("Header")}
+                    </th>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </thead>
+        <tbody
+          {...getTableBodyProps()}
+          className="divide-y divide-gray-200 bg-white"
+        >
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr
+                {...row.getRowProps()}
+                key={row.id}
+                className="transition-all hover:bg-gray-200"
+              >
+                {row.cells.map((cell) => {
+                  const isEditable = (cell.column as CustomColumnInstance)
+                    .isEditable;
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      key={cell.column.id}
+                      className={`py-4 px-6 text-sm ${
+                        isEditable ? "text-blue-600" : "text-gray-500"
+                      }`}
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
