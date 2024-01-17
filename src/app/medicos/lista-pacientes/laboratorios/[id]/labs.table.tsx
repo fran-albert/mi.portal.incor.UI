7f2ea11610d097e8fb812fa9d9fa4fa21e86e76a
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-01-17 14:16:33 -0300

    Refactor LabsTable component layout

--- src/app/medicos/lista-pacientes/laboratorios/[id]/labs.table.tsx
+++ src/app/medicos/lista-pacientes/laboratorios/[id]/labs.table.tsx
@@ -55,77 +55,77 @@ const LabsTable = ({ paciente }: any) => {
 
   return (
     <>
-        <div className="flex justify-center p-4 overflow-x-auto">
-          <div className="w-full lg:w-full">
-            {labs && labs.length > 0 ? (
-              <>
-                <Table
-                  aria-label="Descripción de la tabla"
-                  bottomContent={
-                    <div className="flex w-full justify-center">
-                      <Paginacion
-                        totalItems={totalLabs}
-                        itemsPerPage={itemsPorPagina}
-                        currentPage={paginaActual}
-                        total={totalPages}
-                        setCurrentPage={setPaginaActual}
-                        onChange={handlePageChange}
-                      />
-                    </div>
-                  }
-                  classNames={{
-                    wrapper: "min-h-[222px]",
-                  }}
-                >
-                  <TableHeader columns={columns}>
-                    {(column: ITableColumn) => (
-                      <TableColumn key={column.key}>{column.label}</TableColumn>
-                    )}
-                  </TableHeader>
-                  <TableBody>
-                    {estudiosMostrados &&
-                      estudiosMostrados.map((estudio, index) => (
-                        <TableRow
-                          key={estudio.id}
-                          className="transition-all hover:bg-gray-100 dark:border-black-700 dark:bg-black-800"
-                        >
-                          <TableCell className="py-3 text-gray-900 text-base">
-                            {(paginaActual - 1) * itemsPorPagina + (index + 1)}
-                          </TableCell>
-                          <TableCell className="py-3 text-gray-900 text-base font-medium">
-                            {estudio.name}
-                          </TableCell>
-                          <TableCell className="py-3 text-gray-900 text-base">
-                            {formatDate(estudio.date)}
-                          </TableCell>
-                          <TableCell className="p-4">
-                            <div className="relative flex justify-around items-center gap-1">
-                              <Tooltip content="Ver PDF">
-                                <span className="text-3xl cursor-pointer active:opacity-50">
-                                  <a
-                                    href={`https://incor-ranking.s3.us-east-1.amazonaws.com/storage/laboratorios/${estudio.file}.pdf`}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                  >
-                                    <FaFilePdf size={30} color="#0d9488" />
-                                  </a>
-                                </span>
-                              </Tooltip>
-                            </div>
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                  </TableBody>
-                </Table>
-              </>
-            ) : (
-              <div className="text-center">
-                <p className="text-gray-700 text-lg">
-                  No tiene laboratorios registrados en este momento.
-                </p>
-              </div>
-            )}
-          </div>
+      <div className="flex justify-center overflow-x-auto">
+        <div className="w-full lg:w-full">
+          {labs && labs.length > 0 ? (
+            <>
+              <Table
+                aria-label="Descripción de la tabla"
+                bottomContent={
+                  <div className="flex w-full justify-center">
+                    <Paginacion
+                      totalItems={totalLabs}
+                      itemsPerPage={itemsPorPagina}
+                      currentPage={paginaActual}
+                      total={totalPages}
+                      setCurrentPage={setPaginaActual}
+                      onChange={handlePageChange}
+                    />
+                  </div>
+                }
+                classNames={{
+                  wrapper: "min-h-[222px]",
+                }}
+              >
+                <TableHeader columns={columns}>
+                  {(column: ITableColumn) => (
+                    <TableColumn key={column.key}>{column.label}</TableColumn>
+                  )}
+                </TableHeader>
+                <TableBody>
+                  {estudiosMostrados &&
+                    estudiosMostrados.map((estudio, index) => (
+                      <TableRow
+                        key={estudio.id}
+                        className="transition-all hover:bg-gray-100 dark:border-black-700 dark:bg-black-800"
+                      >
+                        <TableCell className="py-3 text-gray-900 text-base">
+                          {(paginaActual - 1) * itemsPorPagina + (index + 1)}
+                        </TableCell>
+                        <TableCell className="py-3 text-gray-900 text-base font-medium">
+                          {estudio.name}
+                        </TableCell>
+                        <TableCell className="py-3 text-gray-900 text-base">
+                          {formatDate(estudio.date)}
+                        </TableCell>
+                        <TableCell className="p-4">
+                          <div className="relative flex justify-around items-center gap-1">
+                            <Tooltip content="Ver PDF">
+                              <span className="text-3xl cursor-pointer active:opacity-50">
+                                <a
+                                  href={`https://incor-ranking.s3.us-east-1.amazonaws.com/storage/laboratorios/${estudio.file}.pdf`}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                >
+                                  <FaFilePdf size={30} color="#0d9488" />
+                                </a>
+                              </span>
+                            </Tooltip>
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </>
+          ) : (
+            <div className="text-center">
+              <p className="text-gray-700 text-lg">
+                No tiene laboratorios registrados en este momento.
+              </p>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
