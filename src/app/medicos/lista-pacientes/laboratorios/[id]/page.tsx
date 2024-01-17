50b0561b8ce116462345d7f4638796231ac095be
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-01-17 14:16:36 -0300

    Fix import casing in LabsPatientPage

--- src/app/medicos/lista-pacientes/laboratorios/[id]/page.tsx
+++ src/app/medicos/lista-pacientes/laboratorios/[id]/page.tsx
@@ -6,7 +6,7 @@ import useFetchUser from "@/hooks/useFetchUser";
 import LoadingPage from "@/components/Loading";
 import DataLabsTable from "./data.labs.table";
 import HeaderPatient from "@/components/ui/Header";
-// import withSessionTimeout from "@/components/withSessionTimeout";
+import withSessionTimeout from "@/components/WithSessionTimeout";
 function LabsPatientPage({ params }: { params: any }) {
   const idPatient = parseInt(params.id);
   const { session } = useCustomSession();
@@ -18,17 +18,17 @@ function LabsPatientPage({ params }: { params: any }) {
 
   return (
     <div className="flex flex-col md:flex-row">
-      <div className="md:w-48 w-full">
+      <div className="md:w-64 w-full">
         <SideBar />
       </div>
       <div className="flex-grow">
         <HeaderPatient user={user} />
-        <main className="p-4 flex justify-center">
+        <main className="flex-grow p-4 flex justify-center">
           <div className="flex flex-col items-center w-full">
             <div className="w-auto lg:w-4/5">
               <LabsTable paciente={user} />
             </div>
-            <div className="w-auto lg:w-4/5">
+            <div className="w-auto lg:w-4/5 mt-5">
               {/* <DataLabsTable paciente={user} /> */}
             </div>
           </div>
@@ -38,5 +38,4 @@ function LabsPatientPage({ params }: { params: any }) {
   );
 }
 
-// export default withSessionTimeout(Pacientes);
-export default LabsPatientPage;
+export default withSessionTimeout(LabsPatientPage);
