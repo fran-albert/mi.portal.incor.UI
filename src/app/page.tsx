bd3c1cd5449d6cd855289560371d696b96da358a
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-05-06 10:39:12 -0300

    no message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,9 @@ function HomePage() {
       <SideBar />
       <div className="py-12 mt-32">
         <div className="flex flex-wrap justify-center px-4 space-x-0 sm:space-x-8">
-          <div className="w-full md:w-1/2 lg:w-1/3 p-2">
-            <LoginPage />
+          <div className="w-full md:w-1/2 lg:w-1/3 p-2 text-center">
+            {/* <LoginPage /> */}
+            Sitio web en mantenimiento. Disculpe las molestias
           </div>
         </div>
       </div>
